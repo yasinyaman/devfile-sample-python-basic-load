eafb52ebd0d4082de66fcc4c41d8a84c554d9f1d
Yasin YAMAN <99225310+yasinyaman@users.noreply.github.com>
2022-06-03 12:36:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 import os
+from cpu_load_generator import load_all_cores
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    load_all_cores(duration_s=600, target_load=0.5)
     return "Hello World!"
 
 if __name__ == '__main__':

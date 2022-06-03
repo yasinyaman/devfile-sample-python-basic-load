b9c6cee81ac24d5063b1bb2aa16c08e3cc60c5b3
Yasin YAMAN <99225310+yasinyaman@users.noreply.github.com>
2022-06-03 13:07:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def hello():
     return "Hello World!"
 
 @app.route('/test')
-def hello():
+def test():
     load_all_cores(duration_s=60, target_load=0.5)
-    return "Hello World!"
+    return "Test World!"
 
 if __name__ == '__main__':
     port = int(os.environ.get('FLASK_PORT')) or 8080
